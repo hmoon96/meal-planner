375fc4d8accb2a9e6208c54cc879919caaf51f9a
hmoon96 <hlmooney98@gmail.com>
2025-03-11 14:23:42 +0000

    Added Model to plan app

--- plan/admin.py
+++ plan/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MealPlan
 
 # Register your models here.
+
+admin.site.register(MealPlan)
+
+
--- plan/models.py
+++ plan/models.py
@@ -1,5 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class MealPlan(models.Model):
+    user = models.ForeignKey('User', related_name='meal_plans', on_delete=models.CASCADE)
+    week = models.DateField()
+    monday_food = models.TextField()
+    tuesday_food = models.TextField()
+    wednesday_food = models.TextField()
+    thursday_food = models.TextField()
+    friday_food = models.TextField()
+    saturday_food = models.TextField()
+    sunday_food = models.TextField()
+
+
+
+
 
 
