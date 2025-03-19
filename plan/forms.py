8311b1801fb7cc969fc76cfd1027b997959dd17c
hmoon96 <hlmooney98@gmail.com>
2025-03-19 12:27:50 +0000

    Updated the form, so that the date shows a calendar and the input boxes are smalled

--- plan/forms.py
+++ plan/forms.py
@@ -8,3 +8,13 @@ class MealPlanForm(forms.ModelForm):
         fields = ['week', 'monday_food', 'tuesday_food',
                   'wednesday_food', 'thursday_food', 'friday_food',
                   'saturday_food', 'sunday_food']
+        widgets = {
+            'week': forms.DateInput(attrs={'type': 'date'}),
+            'monday_food': forms.Textarea(attrs={'rows': 3}),
+            'tuesday_food': forms.Textarea(attrs={'rows': 3}),
+            'wednesday_food': forms.Textarea(attrs={'rows': 3}),
+            'thursday_food': forms.Textarea(attrs={'rows': 3}),
+            'friday_food': forms.Textarea(attrs={'rows': 3}),
+            'saturday_food': forms.Textarea(attrs={'rows': 3}),
+            'sunday_food': forms.Textarea(attrs={'rows': 3}),
+        }
