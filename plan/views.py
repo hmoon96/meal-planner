aeccd9467d1defc94549d038a4edd1efced0de72
hmoon96 <hlmooney98@gmail.com>
2025-03-21 10:17:29 +0000

    Added message notifications to each of the crud functionalitues

--- plan/views.py
+++ plan/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import MealPlan
 from .forms import MealPlanForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 def Home(request):
@@ -25,12 +26,14 @@ def CreateMeal(request):
             meal_plan = plan.save(commit=False)
             meal_plan.user = request.user
             plan.save()
+            messages.success(request, 'Meal plan created successfully')
             return redirect("meal_view")
         else:
             context = {
                 "form": plan,
                 "errors": plan.errors,  # Pass form errors to the context
             }
+            messages.error(request, 'Your Meal Plan has not been created')
             return render(request, 'plan/create.html', context)
     else:
         form = MealPlanForm()
@@ -46,12 +49,14 @@ def UpdateMeal(request, id):
     if request.method == "POST":
         plan = MealPlanForm(request.POST, instance=meal_to_update)
         plan.save()
+        messages.success(request, 'Meal plan updated successfully')
         return redirect("meal_view")
     else:
         form = MealPlanForm(instance=meal_to_update)
         context = {
             "form": form,
         }
+        messages.error(request, 'Your Meal Plan has not been updated')
         return render(request, 'plan/update.html', context)
 
 
@@ -60,15 +65,24 @@ def DeleteMeal(request, id):
     meal_to_delete = get_object_or_404(MealPlan, id=id, user=request.user)
     if request.method == 'POST':
         meal_to_delete.delete()
+        messages.success(request, 'Meal plan deleted successfully')
         return redirect('meal_view')
     return render(request, 'plan/delete.html')
 
 
 def SignUp(request):
+    if request.method == 'POST':
+        messages.success(request, 'You signed up successfully')
+        return redirect('home')
     return render(request, 'plan/signup.html')
 
 
 def Login(request):
+    if request.method == 'POST':
+        messages.success(request, 'You are now logged in')
+        return redirect('home')
+    else:
+        messages.error(request, 'Invalid username or password')
     return render(request, 'plan/login.html')
 
 
